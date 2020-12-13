660ced9a68e7bc55572450b55b2d82025b1773ea
Alex Rudoy <rudoy4ik@gmail.com>
2020-12-13 18:33:14 +0200

    v1.0

--- app/js/index.js
+++ app/js/index.js
@@ -1,72 +1,15 @@
 import "../scss/main.scss";
+import buttonsFunctions from "./buttonFunctions";
+import utils from "./utils";
 
-let deleteDuplicatesButton = document.getElementById("deleteDuplicatesButton");
-let deleteDuplicatesByDomainButton = document.getElementById("deleteDuplicatesByDomainButton");
-let prepareLinksButton = document.getElementById("prepareLinksButton");
-let getDomainsButton = document.getElementById("getDomainsButton");
-let openAllLinksButton = document.getElementById("openAllLinksButton");
-let linksTextArea = document.getElementById("linksTextArea");
-let filterTextArea = document.getElementById("filterTextArea");
+const linksTextArea = document.getElementById("linksTextArea");
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => button.addEventListener("click", buttonClickHandler));
 
-deleteDuplicatesButton.addEventListener("click", deleteDuplicates);
-deleteDuplicatesByDomainButton.addEventListener("click", deleteDuplicatesByDomain);
-prepareLinksButton.addEventListener("click", prepareLinks);
-getDomainsButton.addEventListener("click", getDomains);
-openAllLinksButton.addEventListener("click", openAllLinks);
-
-function deleteDuplicates() {
-  let links = processTextArea(linksTextArea);
-
-  // actual delete duplicates
-  let linksObject = links.reduce((result, link) => {
-    if (!result[link]) {
-      result[link] = link;
-    }
-    return result;
-  }, {});
-
-  // transform back to array
-  let result = [];
-  for (const key in linksObject) {
-    result.push(linksObject[key]);
-  }
-
-  linksTextArea.value = result.join("\n");
+function buttonClickHandler(e) {
+  const links = utils.getArrayFromTextArea(linksTextArea);
+  const nameOfFunction = e.target.id.replace("Button", "");
+  const functionToCall = buttonsFunctions[nameOfFunction];
+  const aggregatedLinks = functionToCall(links);
+  linksTextArea.value = aggregatedLinks.join("\n");
 }
-
-function deleteDuplicatesByDomain() {
-  let links = processTextArea(linksTextArea);
-
-  // actual delete duplicates
-  let linksObject = links.reduce((result, link) => {
-    let shortLink = cutTheLink(link);
-
-    if (!result[shortLink]) {
-      result[shortLink] = link;
-    }
-    return result;
-  }, {});
-
-  // transform back to array
-  let result = [];
-  for (const key in linksObject) {
-    result.push(linksObject[key]);
-  }
-
-  linksTextArea.value = result.join("\n");
-}
-
-function processTextArea(textarea) {
-  return textarea.value
-    .split("\n")
-    .map((line) => line.trim())
-    .filter((line) => !!line);
-}
-
-function cutTheLink(link) {
-  return link.replace(/(http(s)?:\/\/)?(www.)?/gi, "").replace(/\/.*/gi);
-}
-
-function prepareLinks() {}
-function getDomains() {}
-function openAllLinks() {}
